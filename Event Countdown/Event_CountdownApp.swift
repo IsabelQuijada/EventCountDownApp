aae2ea6318b626d1813a9215e2331841fceeb638
IsabelQuijada <miqc07@outlook.com>
2024-10-06 16:48:32 +0200

    Initial Commit

---
+++ Event Countdown/Event_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_CountdownApp.swift
+//  Event Countdown
+//
+//  Created by Isabel Quijada on 06.10.24.
+//
+
+import SwiftUI
+
+@main
+struct Event_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
