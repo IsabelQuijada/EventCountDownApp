380a54595d3503db722a81c5c6ee707bc510b2b9
IsabelQuijada <miqc07@outlook.com>
2024-10-07 22:15:25 +0200

    Refactor Event Row touse DatePicker for selecting the event's date.

--- Event Countdown/EventRow.swift
+++ Event Countdown/EventRow.swift
@@ -9,19 +9,29 @@ import SwiftUI
 
 struct EventRow: View {
     let event: Event
-
+    @StateObject private var timer = TimerManager()
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(event.title)
-                .font(.headline)
-                .foregroundColor(event.textColor)
-            
-            Text(relativeDate(for: event.date))
-                .font(.subheadline)
-                .foregroundColor(.gray)
+        HStack {
+            VStack(alignment: .leading) {
+                Text(event.title)
+                    .font(.headline)
+                    .foregroundColor(event.textColor)
+                
+                Text(relativeDate(for: event.date))
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+        }
+        .padding(.vertical, 8)
+        .onAppear {
+            timer.start()
+        }
+        .onDisappear {
+            timer.stop()
         }
     }
-
+    
     private func relativeDate(for date: Date) -> String {
         let formatter = RelativeDateTimeFormatter()
         formatter.unitsStyle = .full
@@ -29,6 +39,24 @@ struct EventRow: View {
     }
 }
 
-    #Preview {
-        EventRow(event: .init(id: UUID(), title: "Event Title", date: .init(timeIntervalSince1970: 16760000), textColor: .red))
+class TimerManager: ObservableObject {
+    @Published var currentDate = Date()
+    
+    private var timer: Timer?
+    
+    func start() {
+        stop()
+        timer = Timer.scheduledTimer(withTimeInterval: 60.0, repeats: true) { _ in
+            self.currentDate = Date()
+        }
     }
+    
+    func stop() {
+        timer?.invalidate()
+        timer = nil
+    }
+}
+
+#Preview {
+    EventRow(event: .init(id: UUID(), title: "Event Title", date: .init(timeIntervalSince1970: 16760000), textColor: .red))
+}
